6b6a5082c4713b53d4d7042b069f494de87b0c0e
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-04-13 21:40:23 +0300

    modal close function changes

--- src/js/closeModal.js
+++ src/js/closeModal.js
@@ -1,32 +1,38 @@
 //May change, if the name in HTML will be different
-const backdrop = document.querySelector(".backdrop");
-const modal = document.querySelector(".modal");
+const backdropEl = document.querySelector(".backdrop");
+const closeBtnEl = document.querySelector(".close-button");
+const modalEl = document.querySelector(".modal");
 
 //Hides modal if Escape button is pressed
 export function hideModalOnEscape(e) {
   if (e.code === "Escape") {
     //e.code === 'Escape' && isOpen === true
-    hideBackdrop();
-    removeEventListeners();
+    hideModal();
   }
 }
 
 //Hides modal if clicked outside of the modal
 export function hideModalOnCLick(e) {
-  if (e.target !== modal) {
-    hideBackdrop();
-    removeEventListeners();
+  if (e.target !== modalEl) {
+    hideModal();
   }
   e.stopPropagation();
 }
 
+//HidesModal
+function hideModal() {
+    hideBackdrop();
+    removeEventListeners();
+  }
+
 //Hides backdrop
 function hideBackdrop() {
-  backdrop.classList.add("hidden");
+    backdropEl.classList.add("hidden");
 }
 
 //Removes all event listeners
 function removeEventListeners() {
-  backdrop.removeEventListener("mousedown", hideModalOnEscape);
-  backdrop.removeEventListener("click", hideModalOnCLick);
+    window.removeEventListener("mousedown", hideModalOnEscape);
+    window.removeEventListener("click", hideModalOnCLick);
+    closeBtnEl.removeEventListener("click", hideModalOnCLick);
 }
