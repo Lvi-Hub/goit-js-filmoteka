402d03c65ecc134bd97950c0ba0f4cbf24323218
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-04-18 18:25:08 +0200

    Update modal-about.js

--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -12,11 +12,11 @@ const API_KEY = '481cbb6dba5121edc01136f73aa6b3c6';
 const closeBtnEl = document.querySelector('.movie-card__close-btn');
 const modalEl = document.querySelector('.movie-card');
 const movieTitle = document.querySelector('.movie-card__info__title');
-const text = document.querySelector('.movie-card__info__small-text');
-const popularity = document.querySelector('.movie-card__info__details-text');
+const textEl = document.querySelector('.movie-card__info__small-text');
+const popularityEl = document.querySelector('.movie-card__info__details-text');
 const originalTitle = document.querySelector('.original-title');
-const genres = document.querySelector('.genres');
-const poster = document.querySelector('.movie-card__img');
+const genresEl = document.querySelector('.genres');
+const posterEl = document.querySelector('.movie-card__img');
 
 function fetchFilms(id) {
   return fetch(`${BASE_URL}movie/${id}?api_key=${API_KEY}`).then(response => {
@@ -35,7 +35,7 @@ export function addEventlListenertoFilmCard() {
       card.classList.remove('hidden');
       body.classList.add('hide-scroll');
       const idFilm = event.currentTarget.getAttribute('movie-id');
-      poster.removeAttribute('src');
+      posterEl.removeAttribute('src');
       fetchFilms(idFilm).then(data => {
         fillModal(data);
       });
@@ -55,12 +55,12 @@ function fillModal({
   release_date,
 }) {
   movieTitle.innerHTML = title;
-  text.innerHTML = overview;
-  popularity.innerHTML = popularity;
+  textEl.innerHTML = overview;
+  popularityEl.innerHTML = popularity;
   originalTitle.innerHTML = title;
   genreNames = genres.map(genre => genre.name).join(', ');
-  genres.innerHTML = genreNames;
-  poster.setAttribute('src', 'https://image.tmdb.org/t/p/w500' + poster_path);
+  genresEl.innerHTML = genreNames;
+  posterEl.setAttribute('src', 'https://image.tmdb.org/t/p/w500' + poster_path);
 
   const filmDetails = {
     id,
