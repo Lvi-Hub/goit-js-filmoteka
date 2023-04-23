b5aae735153343d24e7adca9c5bda360ae473814
VSaverskyi <vovikck15@gmail.com>
2023-04-23 10:19:30 +0200

    to find bugs

--- src/js/homepage_main.js
+++ src/js/homepage_main.js
@@ -1,57 +1,59 @@
-import axios from "axios";
+import axios from 'axios';
 import { BASE_URL, API_KEY } from './constants';
-import { addEventlListenertoFilmCard } from "./modal-about";
+import { addEventlListenertoFilmCard } from './modal-about';
 import { showSpinner } from './show_spinner';
-import Pagination from "tui-pagination";
-import { paginationContainer } from "./pagination";
+import Pagination from 'tui-pagination';
+import { paginationContainer } from './pagination';
 
 let pageNumber = 1;
 
 export const moviesGenres = [
-  { id: 28, name: "Action" },
-  { id: 12, name: "Adventure" },
-  { id: 16, name: "Animation" },
-  { id: 35, name: "Comedy" },
-  { id: 80, name: "Crime" },
-  { id: 99, name: "Documentary" },
-  { id: 18, name: "Drama" },
-  { id: 10751, name: "Family" },
-  { id: 14, name: "Fantasy" },
-  { id: 36, name: "History" },
-  { id: 27, name: "Horror" },
-  { id: 10402, name: "Music" },
-  { id: 9648, name: "Mystery" },
-  { id: 10749, name: "Romance" },
-  { id: 878, name: "Science Fiction" },
-  { id: 10770, name: "TV Movie" },
-  { id: 53, name: "Thriller" },
-  { id: 10752, name: "War" },
-  { id: 37, name: "Western" },
-  { id: 10759, name: "Action & Adventure" },
-  { id: 16, name: "Animation" },
-  { id: 35, name: "Comedy" },
-  { id: 80, name: "Crime" },
-  { id: 99, name: "Documentary" },
-  { id: 18, name: "Drama" },
-  { id: 10751, name: "Family" },
-  { id: 10762, name: "Kids" },
-  { id: 9648, name: "Mystery" },
-  { id: 10763, name: "News" },
-  { id: 10764, name: "Reality" },
-  { id: 10765, name: "Sci-Fi & Fantasy" },
-  { id: 10766, name: "Soap" },
-  { id: 10767, name: "Talk" },
-  { id: 10768, name: "War & Politics" },
-  { id: 37, name: "Western" },
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+  { id: 10759, name: 'Action & Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 10762, name: 'Kids' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10763, name: 'News' },
+  { id: 10764, name: 'Reality' },
+  { id: 10765, name: 'Sci-Fi & Fantasy' },
+  { id: 10766, name: 'Soap' },
+  { id: 10767, name: 'Talk' },
+  { id: 10768, name: 'War & Politics' },
+  { id: 37, name: 'Western' },
 ];
 
 const filmsListEl = document.querySelector('.film-list');
 
-
 export async function fetchApi(pageNumber) {
-  const POPULAR_MOVIES = 'language=en-US&sort_by=popularity.desc&include_adult=false';
+  const POPULAR_MOVIES =
+    'language=en-US&sort_by=popularity.desc&include_adult=false';
 
-  const response = await axios.get(`${BASE_URL}discover/movie?api_key=${API_KEY}&${POPULAR_MOVIES}&page=${pageNumber}`);
+  const response = await axios.get(
+    `${BASE_URL}discover/movie?api_key=${API_KEY}&${POPULAR_MOVIES}&page=${pageNumber}`
+  );
   return response.data;
 }
 
@@ -62,23 +64,20 @@ export async function getPopularMovies() {
     const movies = await fetchApi(pageNumber);
 
     const movieData = movies.results;
-    console.log(movies.results);
-
+    // console.log(movies.results);
 
     const options = {
       totalItems: movies.total_results,
       page: pageNumber,
       itemsPerPage: 20,
       centerAlign: true,
-      
+
       visiblePages: 5,
     };
 
     const pagination = new Pagination(paginationContainer, options);
 
-
-
-    pagination.on('beforeMove', async (eventData) => {
+    pagination.on('beforeMove', async eventData => {
       console.log(pagination);
       pageNumber = eventData.page;
       showSpinner();
@@ -112,16 +111,15 @@ export function checkArrlength(arr) {
 
   return changedGenres;
 }
- 
 
 export function makeMarkupPopularMov(movieData) {
-    return movieData.map(({release_date, title, genre_ids, poster_path, id}) => {
-
-      const genresArr = genre_ids.map((id) => getGenreName(id));
+  return movieData
+    .map(({ release_date, title, genre_ids, poster_path, id }) => {
+      const genresArr = genre_ids.map(id => getGenreName(id));
       const genreResult = checkArrlength(genresArr).join(', ');
       if (release_date === undefined) {
-          release_date = '';
-       }
+        release_date = '';
+      }
       return `<li class="movie-item"  movie-id="${id}">
         <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}" 
           alt="movie poster" loading="lazy" class="movie-item__img" />
